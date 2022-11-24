a6d858152cbf81eeeac3acb1b220decf498cdc76
omkar sunari <omkarnitish2006@gmail.com>
2022-11-24 13:07:15 +0530

    changed in server file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express =require("express");
 const cors = require('cors');
 const mongoose =require("mongoose");
 const userroutes=require("./routes/photo_routes")
+const port=process.env.PORT || 3001
 const app =express();
 app.use(cors());
 app.use(express.json());
@@ -11,5 +12,5 @@ const path = require('path')
 app.use('/images', express.static(('images')))
 app.use('/users',userroutes)
 mongoose.connect("mongodb://localhost:27017/uploads2").then((res)=>console.log("connecting")).catch((err)=>console.log(err))
-app.listen(3001,()=>console.log("server started"))
+app.listen(port,()=>console.log("server started"))
 
